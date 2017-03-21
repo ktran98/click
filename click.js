e8eab27f2e2c311ea8c32bbc6351b0cc28ce20c4
ktran98 <peterkhanhtran@gmail.com>
2017-03-21 13:58:06 -0500

    Add files via upload

---
+++ click.js
@@ -0,0 +1,13 @@
+//declare samecolor() here
+function firstdiv(){
+    let doc = document;
+    let section = doc.querrySelector("#samecolor > div");
+
+    div.style.backgroundColor
+    div.style.
+}
+//declare diffcolor() here
+function seconddiv(){
+    let doc = document;
+    let section = doc.querrySelector("differentcolo > div")
+}

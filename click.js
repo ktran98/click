3ea2e691043c81ae372447aa75e0678b7c516450
ktran98 <peterkhanhtran@gmail.com>
2017-03-23 13:53:13 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -1,11 +1,21 @@
 //declare samecolor() here
 function samecolor(){
-    let color = "#00900"
+    const doc = document;
+    let color = "#00900";
 
-    div.style.backgroundColor
+    let divs = doc.QuerrySelectorAll("#samecolor div");
 
+    divs[0].style.backgroundColor = color;
+    divs[0].style.height = "500px"
+    divs[1].style.opacity = 0.75;
 }
 //declare diffcolor() here
 function diffcolor(){
+  const doc = document;
     let color = "#9933s00"
+
+    let divs = doc.QuerrySelector("#diffcolor div");
+
+    
+
 }
